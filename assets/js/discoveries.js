6e5e3624a24f5fae8d0932f99c4137fd2fe04230
Mohamed-mugrabhi <zs2495038@gmail.com>
2026-09-10 02:18:08 +0300

    Update planets project

---
+++ assets/js/discoveries.js
@@ -0,0 +1,17 @@
+function saveDispatch(button) {
+
+    if (button.innerText.includes("Saved")) {
+        button.innerHTML = "♧ Save dispatch";
+    } else {
+        button.innerHTML = "✓ Saved";
+    }
+
+}function saveDispatch(button) {
+
+    if (button.innerText.includes("Saved")) {
+        button.innerHTML = "♧ Save dispatch";
+    } else {
+        button.innerHTML = "✓ Saved";
+    }
+
+}
