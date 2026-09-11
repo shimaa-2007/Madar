1b91b12eb0adc5cb1d120d503014591d8cac45af
rmadanreham689-bit <rmadanreham689@gmail.com>
2026-09-11 18:37:38 +0300

    Update solar_System.js

--- assets/js/solar_System.js
+++ assets/js/solar_System.js
@@ -1,30 +1,19 @@
 const space = document.getElementById("space");
-
 let started = false;
 
 function startJourney() {
-
-    if (started) {
-        return;
-    }
+    if (started) return;
 
     started = true;
-
     space.classList.add("active");
 
     setTimeout(function () {
-
         space.classList.add("system");
-
     }, 3500);
 }
 
 window.addEventListener("load", function () {
-
     setTimeout(function () {
-
         startJourney();
-
     }, 1000);
-
 });
